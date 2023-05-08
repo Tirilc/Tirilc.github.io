efe1b070e83fbc53f164fcbb970bf1c42ece4264
Tirilc <131136955+Tirilc@users.noreply.github.com>
2023-05-08 12:15:21 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const dropDownMenu=document.querySelector('.dropdown_menu')
-const bars = document.querySelector('.bars')
-    
-bars.addEventListener('click', dropdown)
-function dropdown(){
-    bars.classList.toggle('fa-bars')
-    bars.classList.toggle('fa-x')
-    dropDownMenu.classList.toggle('open')
-}
